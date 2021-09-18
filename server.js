9549610be0004f3ae2fb95260fcb4366629a9684
Fayomi <storario2@Gmail.com>
2021-09-18 23:41:07 +0100

    db

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("src/db/db.json");
+const router = jsonServer.router("./db.json");
 const middlewares = jsonServer.defaults({ static: "./build" });
 const port = process.env.PORT || 3000;
 
